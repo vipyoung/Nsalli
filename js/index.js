4bda27864283ba5305048d6abf9b5b42fe47043d
sofiane abbar <sofiane.abbar@gmail.com>
2021-04-05 11:54:41 +0100

    first commit from repo

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    //return [year, month, day].join('-');
+    return [month, day].join('-');
+}
+var today = formatDate();
+$('#time-fejr').text(london[today]['Fejr']);
+$('#time-sunrise').text(london[today]['Sunrise']);
+$('#time-dohr').text(london[today]['Dohr']);
+$('#time-asr').text(london[today]['Asr']);
+$('#time-maghreb').text(london[today]['Maghreb']);
+$('#time-icha').text(london[today]['Icha']);
